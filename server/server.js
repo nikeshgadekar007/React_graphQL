0b732cbe052f093de5e67c1a2b3c1dd5a64a598e
Gadekar <nikesh.gadekar@accenture.com>
2025-04-11 00:01:41 +0800

    resolve merge for client

--- server/server.js
+++ server/server.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-
-const users = [
-  { id: "1", name: "Nikesh", age: 32, isMarried: true },
-  { id: "2", name: "Dipesh", age: 32, isMarried: true },
-  { id: "3", name: "Amit", age: 39, isMarried: true },
-];
-=======
 import {ApolloServer} from '@apollo/server'
 import {startStandaloneServer} from '@apollo/server/standalone';
 
@@ -15,13 +5,11 @@ const users = [
 	{id: "1", name:"Nikesh", age: 32, isMarried: true},
 	{id: "2", name:"Dipesh", age: 32, isMarried: true},
 	{id: "3", name:"Amit", age: 39, isMarried: true}
-]
->>>>>>> 9fca5bf705395927142b12d14df1be047e9f4ca8
+];
 
 const typeDefs = `
 		type Query {
 			getUsers: [User]
-<<<<<<< HEAD
 	    getUserById(id: ID!): User
 
 		}
@@ -30,21 +18,12 @@ const typeDefs = `
           upadateUser(id: ID!, name: String!, age: Int!, isMarried: Boolean!): [User]
           deleteUser(id : ID!) : [User]
             }
-=======
-	        getUserById(id: ID!): User
-
-		}
-		type Mutation {
-			createUser(name: String!, age: Int!, isMarried: Boolean!): User
-		}
->>>>>>> 9fca5bf705395927142b12d14df1be047e9f4ca8
 		type User {
 			id: ID
 			name: String
 			age: Int
 			isMarried: Boolean 
 		}
-<<<<<<< HEAD
 `;
 
 const resolvers = {
@@ -102,43 +81,3 @@ const { url } = await startStandaloneServer(server, {
 });
 
 console.log(`Server running at: ${url}`);
-=======
-`
-
-const resolvers = { 
-	Query: {
-		getUsers: () => {
-			return users;
-		},
-		getUserById: (parent, args) => {
-			const id = args.id;
-			alert('dsadsa', id)
-			return users.find((user) => user.id === id);
-		}
-	},
-	Mutation: {
-		createUser: (parent, args) => {
-
-			const {name, age, isMarried} = args;
-
-			const newUser = {
-				id: (users.length + 1).toString(),
-				name,
-				age,
-				isMarried
-			};
-
-			users.push(newUser);
-
-		}
-	}
-}
-
-const server = new ApolloServer({typeDefs, resolvers});
-
-const {url} = await startStandaloneServer(server, {
-	listen: {port: 4000}
-})
-
-console.log(`Server running at: ${url}`);
->>>>>>> 9fca5bf705395927142b12d14df1be047e9f4ca8
